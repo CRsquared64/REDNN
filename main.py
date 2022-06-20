ad34374e888bef6802ed620fcd5ff47260605521
Callum <Callumr714@icloud.com>
2022-06-20 17:47:08 +0100

    loss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+
 class LayerD:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -9,3 +10,14 @@ class LayerD:
     def fpass(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
+
+class Loss:
+    def calc(self, output, y):
+        s_losses = self.fpass(output, y)
+        d_loss = np.mean(s_losses)
+        return d_loss
+
+class Loss_entropy(Loss):
+    def fpass(self, y_pre, y_tru):
+        samples = len(y_pre)
+        y_pre
