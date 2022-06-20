d1f3be5c91a3b6531c936a92df974aaf13df76b5
Callum <Callumr714@icloud.com>
2022-06-20 22:04:35 +0100

    outputting

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import numpy as np
 import cv2
+import nnfs
+from nnfs.datasets import spiral_data
+
+from activations import Activation_R, Activation_SM
+
+nnfs.init()
 
 
 class LayerD:
@@ -17,12 +23,35 @@ class Loss:
         d_loss = np.mean(s_losses)
         return d_loss
 
+
 class Loss_entropy(Loss):
     def fpass(self, y_pre, y_tru):
         samples = len(y_pre)
-        y_pre_clipped = np.clip(y_pre, 1e-7, 1-1e-7)
+        y_pre_clipped = np.clip(y_pre, 1e-7, 1 - 1e-7)
 
         if len(y_tru.shape) == 1:
             c_confidences = y_pre_clipped[range(samples), y_tru]
         elif len(y_tru.shape) == 2:
-            c_confidences = np.sum(y_pre_clipped*y_tru, axis=1)
+            c_confidences = np.sum(y_pre_clipped * y_tru, axis=1)
+
+        nlog_like = -np.log(c_confidences)
+        return nlog_like
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+layer1 = LayerD(2, 3)
+active1 = Activation_R()
+
+layer2 = LayerD(3, 3)
+active2 = Activation_SM()
+
+layer1.fpass(X)
+active1.fpass(layer1.output)
+
+layer2.fpass(active1.output)
+active2.fpass(layer2.output)
+
+print(active2.output[:5])
+loss_func = Loss_entropy()
+loss = loss_func.calc()
