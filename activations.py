07eb214b41145ee81b9876a70eb1db2e7815a24e
Callum <Callumr714@icloud.com>
2022-06-20 17:43:59 +0100

    Activation_SM

--- activations.py
+++ activations.py
@@ -7,3 +7,9 @@ class Activation_R:
         self.output = np.maximum(0, inputs)
 
 class Activation_SM:
+    def fpass(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        prob = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = prob
+
+
