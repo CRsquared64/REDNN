ed9ee3566cfeb6b0e40af778cb6496d1aba754de
Callum <Callumr714@icloud.com>
2022-06-20 17:40:12 +0100

    inital commit

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import numpy as np
-import cv2 as cv
+import cv2
+
+class LayerD:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def fpass(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
